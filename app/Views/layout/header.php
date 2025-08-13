05147d91afed99c5a2ced78f006eda19b2f7aab8
Mhaew <sittichaithana01@gmail.com>
2025-08-13 14:33:51 +0700

    save my changes

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -240,6 +240,36 @@
     </style>
 
     <style>
+        .row-2col {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 0;
+            margin: 0 -15px;
+        }
+
+        .column {
+            flex: 1 1 50%;
+            padding: 0 10px;
+            box-sizing: border-box;
+        }
+
+        /* เส้นคั่นกลาง */
+        .column:first-child {
+            border-right: 1px solid #ccc;
+        }
+
+        /* รองรับหน้าจอแคบ */
+        @media (max-width: 768px) {
+            .column {
+                flex: 1 1 100%;
+                border-right: none;
+                border-bottom: 1px solid #ccc;
+                margin-bottom: 2rem;
+            }
+        }
+    </style>
+
+    <style>
         .service-features ul {
             list-style: none;
             padding-left: 0;
@@ -327,12 +357,15 @@
             flex: 1;
         }
 
-
         .line-black {
             height: 2px;
             background-color: black;
-            width: 100%;
+            width: 10vw;
+            /* viewport width */
             margin: 20px 0;
+            position: relative;
+            left: -15vw;
+            /* แก้ให้เริ่มจากซ้ายสุด */
         }
 
         .item-break {
@@ -473,7 +506,7 @@
                 <!-- <svg class="my-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="bgCarrier" stroke-width="0"></g>
                     <g id="tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                    <g id="iconCarrier">
+                    < id="iconCarrier">
                         <path d="M22 22L2 22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
                         <path d="M17 22V6C17 4.11438 17 3.17157 16.4142 2.58579C15.8284 2 14.8856 2 13 2H11C9.11438 2 8.17157 2 7.58579 2.58579C7 3.17157 7 4.11438 7 6V22" stroke="currentColor" stroke-width="1.5"></path>
                         <path opacity="0.5" d="M21 22V8.5C21 7.09554 21 6.39331 20.6629 5.88886C20.517 5.67048 20.3295 5.48298 20.1111 5.33706C19.6067 5 18.9045 5 17.5 5" stroke="currentColor" stroke-width="1.5"></path>
@@ -489,7 +522,7 @@
                         <path opacity="0.5" d="M10 15H14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
                         <path d="M12 9V5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                         <path d="M14 7L10 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </g>
+                    </
                 </svg> -->
 
                 <h1 class="sitename">โรงพยาบาลพิชัย</h1>
